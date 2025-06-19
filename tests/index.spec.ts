d8872219917014025258ab864b97ac0f3ed01628
Dmitry Romashov <framebassman@gmail.com>
2025-06-19 12:54:28 +0200

    Add another test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('should work', async ({ request }) => {
+test('check healthcheck', async ({ request }) => {
   const response = await request.get(`/healthcheck`);
   expect(response.ok()).toBeTruthy();
-})
+});
+
+test('check crm routes', async ({ request }) => {
+  const response = await request.get(`/collections/todos`);
+  expect(response.ok()).toBeTruthy();
+});
+
