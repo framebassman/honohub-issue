555ef14f6716f68d1212204f825ca6fcec6791af
Dmitry Romashov <framebassman@gmail.com>
2025-06-19 13:08:20 +0200

    Fix

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,11 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('check healthcheck', async ({ request }) => {
   const response = await request.get(`/healthcheck`);
-  expect(response.text()).toBe('OK');
+  expect(await response.text()).toBe('OK');
 });
 
 test('check crm routes', async ({ request }) => {
   const response = await request.get(`/collections/todos`);
-  expect(response.text()).toBe('OK');
+  expect(await response.text()).toBe('OK');
 });
 
