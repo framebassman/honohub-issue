76cc3612f15680b3845c6fa653eba12deda43a60
Dmitry Romashov <framebassman@gmail.com>
2025-06-29 16:22:19 +0200

    Fix

--- hub.config.ts
+++ hub.config.ts
@@ -3,8 +3,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { defineCollection, defineHub } from "honohub";
 import * as schema from "./src/db/schema";
 
-// const neonSql = neon("postgresql://neondb_owner:npg_ZgaySezBR43Y@ep-nameless-dream-a203aqj4-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
-const neonSql = neon("postgres://test:test@localhost:5432/test");
+const neonSql = neon("postgresql://neondb_owner:npg_ZgaySezBR43Y@ep-nameless-dream-a203aqj4-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
 const db = drizzle(neonSql, { schema });
 
 export default defineHub({
